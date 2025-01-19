cc9f91f98fc5a6bc8f38fe89949768ccdf246f7a
Isitha Tennakoon <82481046+IsithaT@users.noreply.github.com>
2025-01-19 00:45:41 -0800

    added median tool

--- server/functions.py
+++ server/functions.py
@@ -20,5 +20,26 @@ AItools = [
         },
         "strict": True
     }
+}, 
+{
+    "type": "function",
+    "function": {
+        "name": "calculateMedian",
+        "description": "Calculate the median of a given column in a CSV file.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "colName": {
+                    "type": "string",
+                    "description": "The name of the column for which the median should be calculated."
+                }
+            },
+            "required": [
+                "colName"
+            ],
+            "additionalProperties": False
+        },
+        "strict": True
+    }
 }
 ]
